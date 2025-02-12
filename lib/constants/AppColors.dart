c64bdd9124887eefd170249fadc191b70e99dc21
peexforge <peexforge@gmail.com>
2025-02-12 10:14:28 +0330

    Created colors class

---
+++ lib/constants/AppColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const Color black = Color(0xFF1A1A1A); // Background
+  static const Color darkPurple = Color(0xFF281B45); // UI Elements
+  static const Color purple = Color(0xFF362763); // Texts
+  static const Color lightPurple = Color(0xFF443381); // Important ELements
+  static const Color lightestPurple = Color(0xFF523F9F); // Bold Elements
+}
