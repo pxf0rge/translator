44b52f4657ac482c44abae7b8108932f05c3def8
peexforge <peexforge@gmail.com>
2025-02-13 14:31:59 +0330

    Added Language selector row

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,8 +21,56 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _getHomeBody() {
-    return Column(
-      children: [],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _langBtn("First Lang", true),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.compare_arrows_rounded),
+                color: AppColors.others,
+              ),
+              _langBtn("Second Lang", false),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _langBtn(String title, bool isFirstLanguage) {
+    return Container(
+      width: 160,
+      height: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: AppColors.elements,
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            children: [
+              Text(
+                "$title",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+              Icon(
+                Icons.arrow_drop_down,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -30,13 +78,14 @@ class _HomePageState extends State<HomePage> {
     return AppBar(
       elevation: 0,
       title: Text(
-        "Translate",
+        "Translator Application",
         style: TextStyle(
           fontSize: 18,
           fontWeight: FontWeight.bold,
           color: Colors.white,
         ),
       ),
+      centerTitle: true,
       backgroundColor: AppColors.elements,
     );
   }
