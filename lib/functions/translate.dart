7b0efa49d1afce9bb7259317b91c9d3f77fb0e0c
peexforge <peexforge@gmail.com>
2025-02-14 13:35:16 +0330

    Added await for translation in home page

--- lib/functions/translate.dart
+++ lib/functions/translate.dart
@@ -1,6 +1,19 @@
-String translate(
-    {required String sourceLang,
-    required String targetLang,
-    required String text}) {
-  return "";
+import 'package:dio/dio.dart';
+import 'package:translator/data/Requests.dart';
+
+Requests requests = Requests();
+
+Future<String> tr({
+  required String sourceLang,
+  required String targetLang,
+  required String text,
+  required String apiToken,
+}) async {
+  Response response = await requests.post(
+    url: 'https://api.one-api.ir/translate/v1/microsoft',
+    headers: {'one-api-token': apiToken},
+    data:
+        '{"source": "$sourceLang" , "target": "$targetLang" , "text": "$text"}',
+  );
+  return response.data;
 }
