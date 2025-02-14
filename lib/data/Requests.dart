ef606020f5e6ccfad11654b0087dc8ecf0f1fb65
peexforge <peexforge@gmail.com>
2025-02-14 13:32:00 +0330

    Added POST method to Requests class

--- lib/data/Requests.dart
+++ lib/data/Requests.dart
@@ -1,8 +1,29 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
 final class Requests {
+  final Dio dio = Dio();
+
   Future<Response> get({required String url}) async {
-    Response response = await Dio().get(url);
+    Response response = await dio.get(url);
+    return response;
+  }
+
+  Future<Response> post({
+    required String url,
+    Object? data,
+    Map<String, dynamic>? headers,
+  }) async {
+    Options options = Options();
+    if (headers != null) {
+      options.headers = headers;
+    }
+    if (data != null) {
+      data = jsonEncode(data);
+    }
+    Response response = await dio.post(url, data: data, options: options);
+    print(response.data);
     return response;
   }
 }
