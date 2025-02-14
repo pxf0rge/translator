b4d9c1694a9a2f1b1589c418ed1f61e67dec3318
peexforge <peexforge@gmail.com>
2025-02-14 13:34:13 +0330

    Completed Translation API and Connected to Google

--- lib/data/TranslationAPI.dart
+++ lib/data/TranslationAPI.dart
@@ -1,11 +1,19 @@
+import 'package:translator/functions/translate.dart';
+
 final class TranslationAPI {
   final String apiToken;
 
   TranslationAPI({required this.apiToken});
 
-  String trasnlate(
-          {required String source,
-          required String target,
-          required String text}) =>
-      trasnlate(source: source, target: target, text: text);
+  Future<String> trasnlate({
+    required String source,
+    required String target,
+    required String text,
+  }) async =>
+      await tr(
+        sourceLang: source,
+        targetLang: target,
+        text: text,
+        apiToken: this.apiToken,
+      );
 }
